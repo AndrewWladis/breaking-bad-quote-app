a7eeb35e174ade34112452f04ecc748b6b22e5c8
Andrew Wladis <andrewwladis@Andrews-MacBook-Pro.local>
2023-03-30 16:36:26 -0400

    Small fixes

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
   return (
     <NavigationContainer>
